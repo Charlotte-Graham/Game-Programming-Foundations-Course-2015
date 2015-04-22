e83861904087ed1a4f86a7bf9b9b36ba31067b61
Charlotte-Graham <ziggybusta@hotmail.com>
2015-04-22 20:58:12 +1000

    My First Backup of "Exploding Minds Full of Coffee"
    This is my first backup of the second game we are making for the (Game
    Programming Foundations Course) which I have called “Exploding Minds
    Full of Coffee”

---
+++ Start of "Exploding Minds Full of Coffee"/main.js
@@ -0,0 +1,73 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+
+var startFrameMillis = Date.now();
+var endFrameMillis = Date.now();
+
+function getDeltaTime()
+{
+	endFrameMillis = startFrameMillis;
+	startFrameMillis = Date.now();
+
+	var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
+
+	if(deltaTime > 1)
+		deltaTime = 1;
+		
+	return deltaTime;
+}
+
+var fps = 0;
+var fpsCount = 0;
+var fpsTime = 0;
+
+var player = new Player();
+var keyboard = new Keyboard();
+
+function run()
+{
+	context.fillStyle = "#66CCFF";		
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	var deltaTime = getDeltaTime();
+	
+	player.update(deltaTime);
+	player.draw();
+
+	fpsTime += deltaTime;
+	fpsCount++;
+	if(fpsTime >= 1)
+	{
+		fpsTime -= 1;
+		fps = fpsCount;
+		fpsCount = 0;
+	}		
+
+	context.fillStyle = "#000099";
+	context.font="14px Lucida Blackletter";
+	context.fillText("FPS: " + fps, 5, 20, 100);
+}
+
+//Heya Stupid, Don't touch this stuff//
+(function() {
+  var onEachFrame;
+  if (window.requestAnimationFrame) {
+    onEachFrame = function(cb) {
+      var _cb = function() { cb(); window.requestAnimationFrame(_cb); }
+      _cb();
+    };
+  } else if (window.mozRequestAnimationFrame) {
+    onEachFrame = function(cb) {
+      var _cb = function() { cb(); window.mozRequestAnimationFrame(_cb); }
+      _cb();
+    };
+  } else {
+    onEachFrame = function(cb) {
+      setInterval(cb, 1000 / 60);
+    }
+  }
+  
+  window.onEachFrame = onEachFrame;
+})();
+
+window.onEachFrame(run);
